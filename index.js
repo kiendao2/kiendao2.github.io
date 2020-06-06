ccb15b01788e8c9e295a0e4590fd1fe28556685f
kiendao515 <daotrungkien515@gmail.com>
2020-06-06 23:55:39 +0700

    sua lai footer

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ $(document).ready(function () {
 function checkPw(form) {
     pw1 = form.pw1.value;
     pw2 = form.pw2.value;
-    fname=form.fname.value;
     if (pw1 != pw2) {
     alert ("\nNhập đúng mật khẩu để tiếp tục !")
     return false;
     }
     else return alert('Để lại lời nhắn thành công !');
 }
+setTimeout(function(){
+    alert("Để lại lời nhắn phía cuối trang :< ");
+}, 10000);
