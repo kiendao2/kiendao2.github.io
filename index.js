06602b80077f88d7cfce50042c14ce854d743229
kiendao515 <daotrungkien515@gmail.com>
2020-06-06 23:31:07 +0700

    hoan thanh footer

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ $(document).ready(function () {
         alert('Dang ki thanh cong !');
     });
 });
+function checkPw(form) {
+    pw1 = form.pw1.value;
+    pw2 = form.pw2.value;
+    fname=form.fname.value;
+    if (pw1 != pw2) {
+    alert ("\nNhập đúng mật khẩu để tiếp tục !")
+    return false;
+    }
+    else return alert('Để lại lời nhắn thành công !');
+}
